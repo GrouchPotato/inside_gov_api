3c569c480009473069c3c686b35bb6a8bffd4121
Will Tomlins <will@tomlins.org.uk>
2014-02-06 15:18:03 +0000

    Rails

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+source 'https://BnrJb6FZyzspBboNJzYZ@gem.fury.io/govuk/'
+
+gem 'rails', '4.0.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'debugger'
+end
+
+group :test do
+  # gem 'cucumber-rails', require: false
+  gem 'rspec-rails'
+  gem 'webmock'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InsideGovApi::Application.config.secret_key_base = '98774d4021bedefdcc06faa5161cb34528cf14a3f3e2e959ac25fec0ff47c28cc766c43d7f8bd40fa80b3f8aeb38c4fd1e9f33e1e4b425fef295030e1bb32ba8'
